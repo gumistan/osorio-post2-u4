83ce88673d12a95eca9c88ef2bbfcf6a2831f2a4
gumistan <mariajoseoo@ufps.edu.co>
2026-04-14 06:23:34 -0500

    macros.asm actualizacion

--- macros.asm
+++ macros.asm
@@ -1,9 +1,9 @@
-; macros.asm — biblioteca de macros utilitarias [cite: 28, 29]
+; macros.asm — biblioteca de macros utilitarias 
 
 %macro fin_dos 0
     mov ax, 4C00h
     int 21h
-%endmacro [cite: 30, 31, 32]
+%endmacro 
 
 %macro nueva_linea 0
     mov ah, 02h
@@ -11,24 +11,24 @@
     int 21h
     mov dl, 0Ah
     int 21h
-%endmacro [cite: 40, 42, 44, 45, 46, 47, 48]
+%endmacro 
 
 %macro print_str 1
     mov ah, 09h
     mov dx, %1
     int 21h
-%endmacro [cite: 51, 53, 55, 56, 58]
+%endmacro 
 
 %macro print_char 1
     mov ah, 02h
     mov dl, %1
     int 21h
-%endmacro [cite: 61, 63, 65, 66, 67]
+%endmacro 
 
 %macro leer_char 0
     mov ah, 07h
     int 21h
-%endmacro [cite: 70, 71, 72, 73]
+%endmacro 
 
 %macro repetir_str 2
     mov cx, %2
@@ -37,7 +37,7 @@
     mov dx, %1
     int 21h
     loop %%ciclo
-%endmacro [cite: 80, 82, 83, 85, 87, 94, 95, 96]
+%endmacro 
 
 %macro print_digito 0
     push ax
@@ -47,4 +47,4 @@
     mov dl, al
     int 21h
     pop ax
-%endmacro [cite: 100, 104, 105, 106, 107, 109, 111, 112, 114, 115]
+%endmacro 
